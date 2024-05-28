24e7bf6895aee47e14d8c294c3d66ecac4411805
Ikboljon <32596402+iqboladahamjonov@users.noreply.github.com>
2024-05-28 13:04:12 +0000

    Factory Exercise

--- src/main/java/com/example/VendingMachine.java
+++ src/main/java/com/example/VendingMachine.java
@@ -1,10 +1,25 @@
 package com.example;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class VendingMachine {
 
-    Snack getSnack(String snackType) {
-        return null;
+    private static final Map<String, Supplier<Snack>> drinkMap = new HashMap();
+
+    static {
+        drinkMap.put("ChocolateBar", () -> new ChocolateBar());
+        drinkMap.put("Chips", () -> new Chips());
+        drinkMap.put("Drink", () -> new Drink());
     }
 
+    Snack getSnack(String snackType) {
+        if (drinkMap.get(snackType) != null) {
+            return drinkMap.get(snackType).get();
+        } else {
+            throw new IllegalArgumentException("Invalid snack type: " + snackType);
+        }
+    }
 
 }
